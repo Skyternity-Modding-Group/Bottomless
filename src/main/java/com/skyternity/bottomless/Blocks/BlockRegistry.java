d8699ff7a32f78011888732d2e4eba0ba6ba3b85
LePichu <ishat.prsnl65844@gmail.com>
2021-02-25 12:13:38 +0530

    initial + stone

---
+++ src/main/java/com/skyternity/bottomless/Blocks/BlockRegistry.java
@@ -0,0 +1,24 @@
+package com.skyternity.bottomless.Blocks;
+
+import com.skyternity.bottomless.BottomlessMain;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Block;
+import static net.minecraft.block.Blocks.*;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class BlockRegistry {
+    // Bwocks
+    public static final Block MIDSTONE = register(new Block(FabricBlockSettings.copyOf(STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES,5)), "midstone");
+    public static final Block SHADESTONE = register(new Block(FabricBlockSettings.copyOf(STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES,5)), "shadestone");
+
+    // Methods
+    private static Block register(Block block, String name) {
+        return Registry.register(Registry.BLOCK, new Identifier(BottomlessMain.MOD_ID, name), block);
+    }
+
+    public static void init() {
+        // just loads the class
+    }
+}
---
+++ src/main/java/com/skyternity/bottomless/Items/ItemRegistry.java
@@ -0,0 +1,35 @@
+package com.skyternity.bottomless.Items;
+
+import com.skyternity.bottomless.Blocks.BlockRegistry;
+import com.skyternity.bottomless.BottomlessMain;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ItemRegistry {
+    private static final ItemGroup GROUP = FabricItemGroupBuilder.build(new Identifier(BottomlessMain.MOD_ID,"group"), () -> new ItemStack(BlockRegistry.MIDSTONE));
+
+    // Items
+
+
+    // Blockitems
+    public static final Item MIDSTONE = register(BlockRegistry.MIDSTONE, "midstone", new Item.Settings().group(GROUP));
+    public static final Item SHADESTONE = register(BlockRegistry.SHADESTONE, "shadestone", new Item.Settings().group(GROUP));
+
+    // Methods
+    public static void init () {
+        // Loads Class(TM)
+    }
+    private static Item register(Item item, String name) {
+        return Registry.register(Registry.ITEM, new Identifier(BottomlessMain.MOD_ID, name), item);
+    }
+
+    private static Item register(Block block, String name, Item.Settings settings) {
+        return register(new BlockItem(block, settings), name);
+    }
+}
