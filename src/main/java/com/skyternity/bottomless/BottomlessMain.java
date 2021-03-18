bbe17434ba3872eb36754b4bb1afefea1555fe3a
Diego Sesmas <diegos7167@gmail.com>
2021-03-17 21:49:37 -0700

    Removed unneeded obj loader

--- src/main/java/com/skyternity/bottomless/BottomlessMain.java
+++ src/main/java/com/skyternity/bottomless/BottomlessMain.java
@@ -1,6 +1,5 @@
 package com.skyternity.bottomless;
 
-import com.skyternity.bottomless.Models.ModelReg;
 import com.skyternity.bottomless.blocks.BlockRegistry;
 import com.skyternity.bottomless.items.ItemRegistry;
 import com.skyternity.bottomless.worldgen.dimensions.DimensionRegistry;
@@ -26,7 +25,6 @@ public class BottomlessMain implements ModInitializer {
         BlockRegistry.init();
         DimensionRegistry.init();
         FeatureRegistry.init();
-        ModelReg.init();
     }
 
     public static void log(Level level, String message){
