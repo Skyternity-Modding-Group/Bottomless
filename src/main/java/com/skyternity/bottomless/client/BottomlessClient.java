7ab8fdb8084889a647d7b052e04a912216425d01
LazTheArtist <kingofcreepersyt@gmail.com>
2021-04-24 12:14:40 +0200

    fixed ya crystal rendering

--- src/main/java/com/skyternity/bottomless/client/BottomlessClient.java
+++ src/main/java/com/skyternity/bottomless/client/BottomlessClient.java
@@ -13,8 +13,8 @@ public class BottomlessClient implements ClientModInitializer {
     }
 
     private static void registerBlockRenderLayers() {
-        BlockRenderLayerMap.INSTANCE.putBlock(BlockRegistry.CRYSTAL, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlockRegistry.SMALL_CRYSTAL, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlockRegistry.CRYSTAL_PILLAR, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockRegistry.CRYSTAL, RenderLayer.getCutoutMipped());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockRegistry.SMALL_CRYSTAL, RenderLayer.getCutoutMipped());
+        BlockRenderLayerMap.INSTANCE.putBlock(BlockRegistry.CRYSTAL_PILLAR, RenderLayer.getCutoutMipped());
     }
 }
