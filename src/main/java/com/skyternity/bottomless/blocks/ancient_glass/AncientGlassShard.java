8ef1359422d7907ec3519479bedd5ae592f83e6b
LazTheArtist <kingofcreepersyt@gmail.com>
2021-05-06 01:31:55 +0200

    ancient glass shards are now placeable on every face of a block

--- src/main/java/com/skyternity/bottomless/blocks/ancient_glass/AncientGlassShard.java
+++ src/main/java/com/skyternity/bottomless/blocks/ancient_glass/AncientGlassShard.java
@@ -1,21 +1,40 @@
 package com.skyternity.bottomless.blocks.ancient_glass;
 
-import com.skyternity.bottomless.blocks.BlockRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.WallMountedBlock;
+import net.minecraft.block.enums.WallMountLocation;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
 
-public class AncientGlassShard extends Block {
+public class AncientGlassShard extends WallMountedBlock {
+
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+    public static final EnumProperty<WallMountLocation> FACE = Properties.WALL_MOUNT_LOCATION;
+
+    protected static final VoxelShape CEILING_X_SHAPE = Block.createCuboidShape(6.0D, 14.0D, 5.0D, 10.0D, 16.0D, 11.0D);
+    protected static final VoxelShape CEILING_Z_SHAPE = Block.createCuboidShape(5.0D, 14.0D, 6.0D, 11.0D, 16.0D, 10.0D);
+    protected static final VoxelShape FLOOR_X_SHAPE = Block.createCuboidShape(6.0D, 0.0D, 5.0D, 10.0D, 2.0D, 11.0D);
+    protected static final VoxelShape FLOOR_Z_SHAPE = Block.createCuboidShape(5.0D, 0.0D, 6.0D, 11.0D, 2.0D, 10.0D);
+    protected static final VoxelShape NORTH_SHAPE = Block.createCuboidShape(5.0D, 6.0D, 14.0D, 11.0D, 10.0D, 16.0D);
+    protected static final VoxelShape SOUTH_SHAPE = Block.createCuboidShape(5.0D, 6.0D, 0.0D, 11.0D, 10.0D, 2.0D);
+    protected static final VoxelShape WEST_SHAPE = Block.createCuboidShape(14.0D, 6.0D, 5.0D, 16.0D, 10.0D, 11.0D);
+    protected static final VoxelShape EAST_SHAPE = Block.createCuboidShape(0.0D, 6.0D, 5.0D, 2.0D, 10.0D, 11.0D);
 
     protected static final VoxelShape SHAPE = Block.createCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 7.0D, 12.0D);
 
     public AncientGlassShard(Settings settings) {
         super(settings);
+        this.setDefaultState((BlockState)((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.NORTH)).with(FACE, WallMountLocation.WALL)));
     }
 
     @Override
@@ -23,14 +42,6 @@ public class AncientGlassShard extends Block {
         return SHAPE;
     }
 
-    @Override
-    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        if(world.getBlockState(pos.down()).isSolidBlock(world, pos)){
-            return true;
-        }else{
-            return false;
-        }
-    }
 
     @Override
     public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
@@ -38,6 +49,10 @@ public class AncientGlassShard extends Block {
             world.breakBlock(pos, true);
             world.updateNeighbor(pos, world.getBlockState(pos).getBlock(), pos.up());
         }
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{FACE, FACING});
     }
 
 }
