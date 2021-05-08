c23f17e63c1ea04b9142eede00504bb8e013c4cf
LazTheArtist <kingofcreepersyt@gmail.com>
2021-05-08 13:11:48 +0200

    included geckolib

--- src/main/java/skyternity/bottomless/BottomlessMod.java
+++ src/main/java/skyternity/bottomless/BottomlessMod.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import skyternity.bottomless.main.Registration;
+import software.bernie.geckolib3.GeckoLib;
 
 
 @Mod(BottomlessMod.MOD_ID)
@@ -25,6 +26,7 @@ public class BottomlessMod
 
     public BottomlessMod() {
 
+        GeckoLib.initialize();
         Registration.register();
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
